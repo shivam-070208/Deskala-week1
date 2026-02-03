66275095d136c25adc24aee7092bd2900229f77c
shvgpt-070208 <shvgpt070208@gmail.com>
2026-02-04 00:55:21 +0530

    dsa/level9/easy

---
+++ dsa-homework/level9/easy/delete-duplicate.ts
@@ -0,0 +1,32 @@
+import { ListNode } from "../types/list-node";
+//* https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+/*
+ * This function removes duplicates from a sorted singly linked list.
+ * Given the head of a sorted singly linked list, it deletes all duplicates such that each element appears only once and returns the new head.
+ *
+ * Example:
+ * Input: 1 -> 1 -> 2 -> 3 -> 3
+ * Output: 1 -> 2 -> 3
+ *
+ * Time Complexity:
+ * O(n) - We iterate through the list once.
+ *
+ * Space Complexity:
+ * O(1) - The operation is done in-place using constant extra space.
+ */
+
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let prev:ListNode|null = null;
+    let curr :ListNode|null= head;
+    while (curr) {
+        if (prev?.val !== curr.val) {
+            prev = curr;
+            curr = curr.next;
+        } else {
+            prev.next = curr.next;
+            curr = curr.next;
+        }
+    }
+    return head;
+};
---
+++ dsa-homework/level9/easy/reverse-list.ts
@@ -0,0 +1,38 @@
+import { ListNode } from "../types/list-node";
+//* https://leetcode.com/problems/reverse-linked-list/
+/*
+ * This function reverses a singly linked list.
+ * Given the head of a singly linked list, it reverses the list and returns the new head.
+ *
+ * Example:
+ * Input: 1 -> 2 -> 3 -> 4 -> 5
+ * Output: 5 -> 4 -> 3 -> 2 -> 1
+ *
+ * Time Complexity:
+ * O(n) - We iterate through the list once.
+ *
+ * Space Complexity:
+ * O(n) - The reversal is done by crwating new List.
+ */
+
+
+
+
+
+
+function reverseList(head: ListNode | null): ListNode | null {
+    let reversedList:ListNode|null = null;
+    let curr:ListNode|null = head;
+    while(curr){
+            reversedList = new ListNode(curr.val,reversedList);
+            curr = curr.next;
+    }
+    return reversedList;
+};
+let reversed = reverseList(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))));
+let result: number[] = [];
+while (reversed) {
+    result.push(reversed.val);
+    reversed = reversed.next;
+}
+console.log(result);
